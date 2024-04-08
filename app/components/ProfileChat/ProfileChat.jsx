c70817f10d360015a34b98175aecfc86d6ea64ef
foxenko <ilyabro65410@gmail.com>
2024-04-09 04:04:05 +0500

    1

--- app/components/ProfileChat/ProfileChat.jsx
+++ app/components/ProfileChat/ProfileChat.jsx
@@ -9,6 +9,10 @@ export const ProfileChat = () => {
           src="/images/profilePicture.jpeg"
           alt="profilePicture"
         />
+        <div className={Styles["info"]}>
+          <h3 className={Styles["profileName"]}>Phillip Du</h3>
+          <p className={Styles["lastMessage"]}>Кароче, Василь иди нахуй!!</p>
+        </div>
       </div>
     </div>
   );
